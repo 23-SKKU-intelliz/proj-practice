dbe9a811b5b3c767e99fc27a6b486ecb937916fd
Yikanghoon <hoon5083@gmail.com>
2023-06-01 05:18:54 +0900

    feat: model 추가

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,3 +2,21 @@ from django.db import models
 
 
 # Create your models here.
+class User (models.Model):
+    username = models.CharField(max_length=20)
+    email = models.EmailField()
+
+
+class Article (models.Model):
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=20000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment (models.Model):
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
