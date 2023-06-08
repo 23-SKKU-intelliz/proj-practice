81c0c097aa4a079660d70cd8ac06140a3f76e406
Yikanghoon <hoon5083@gmail.com>
2023-06-09 06:59:26 +0900

    feat: backend boilerplate

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,31 +2,3 @@ from django.db import models
 
 
 # Create your models here.
-class User (models.Model):
-    username = models.CharField(max_length=20)
-    email = models.EmailField()
-
-    class Meta:
-        verbose_name = 'user'
-
-
-class Article (models.Model):
-    title = models.CharField(max_length=200, default='untitled')
-    content = models.TextField(default='empty')
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, related_name='articles',on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = 'article'
-
-
-class Comment (models.Model):
-    content = models.TextField(default='empty')
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
-    article = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = 'comment'
