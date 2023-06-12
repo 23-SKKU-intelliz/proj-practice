e255a6a8500cf149c82f8ac8555fbdbcc322cf69
Yikanghoon <hoon5083@gmail.com>
2023-06-13 05:01:24 +0900

    feat: docker-compose 이용 개발 환경 구성

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if env("APP_ENV") == "development" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
 TIME_ZONE = 'Asia/Seoul'
 
